e2c1748aa4cfbd7fbed574b3fae3f87e0e24b3ca
sunpeng <sunpeng@example.com>
2021-10-26 20:47:45 +0800

    初始配置信息

--- mockito/src/test/java/me/sunpeng/MockitoTest.java
+++ mockito/src/test/java/me/sunpeng/MockitoTest.java
@@ -5,14 +5,16 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatcher;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 /**
  * @author sp
@@ -25,41 +27,41 @@ public class MockitoTest {
     @Test
     public void Test01() {
         //模拟创建一个List对象
-        List<Integer> mock = Mockito.mock(List.class);
+        List<Integer> mock = mock(List.class);
         //调用mock对象的方法
         mock.add(1);
         mock.clear();
         //验证方法是否执行
-        Mockito.verify(mock).add(1);
-        Mockito.verify(mock).clear();
-        Mockito.verify(mock).add(2);
+        verify(mock).add(1);
+        verify(mock).clear();
+        verify(mock).add(2);
     }
 
     @Test
     public void Test02() {
-        LinkedList mockedList = Mockito.mock(LinkedList.class);
-        Mockito.when(mockedList.get(0)).thenReturn("first");
-        Mockito.when(mockedList.get(1)).thenThrow(new RuntimeException());
+        LinkedList mockedList = mock(LinkedList.class);
+        when(mockedList.get(0)).thenReturn("first");
+        when(mockedList.get(1)).thenThrow(new RuntimeException());
         System.out.println(mockedList.get(0));
         System.out.println(mockedList.get(1));
         System.out.println(mockedList.get(999));
-        Mockito.verify(mockedList, Mockito.atLeast(2)).get(0);
+        verify(mockedList, atLeast(2)).get(0);
     }
 
     @Test(expected = IOException.class)//期望报io异常
     public void Test03() throws IOException {
-        OutputStream mock = Mockito.mock(OutputStream.class);
+        OutputStream mock = mock(OutputStream.class);
         //预设当流关闭时候抛出异常
-        Mockito.doThrow(new IOException()).when(mock).close();
+        doThrow(new IOException()).when(mock).close();
         mock.close();
     }
 
     @Test
     public void deepstubsTest() {
-        A a = Mockito.mock(A.class);
-        B b = Mockito.mock(B.class);
-        Mockito.when(a.getB()).thenReturn(b);
-        Mockito.when(b.getName()).thenReturn("Beijing");
+        A a = mock(A.class);
+        B b = mock(B.class);
+        when(a.getB()).thenReturn(b);
+        when(b.getName()).thenReturn("Beijing");
         Assert.assertEquals("Beijing", a.getB().getName());
 
 
@@ -67,17 +69,17 @@ public class MockitoTest {
 
     @Test
     public void deepstubsTest2() {
-        A a = Mockito.mock(A.class, Mockito.RETURNS_DEEP_STUBS);
-        Mockito.when(a.getB().getName()).thenReturn("Beijing");
+        A a = mock(A.class, RETURNS_DEEP_STUBS);
+        when(a.getB().getName()).thenReturn("Beijing");
         Assert.assertEquals("Beijing", a.getB().getName());
     }
 
     @Test
     public void with_arguments() {
-        B b = Mockito.mock(B.class);
+        B b = mock(B.class);
         //预设根据不同的参数返回不同的结果
-        Mockito.when(b.getSex(1)).thenReturn("男");
-        Mockito.when(b.getSex(2)).thenReturn("女");
+        when(b.getSex(1)).thenReturn("男");
+        when(b.getSex(2)).thenReturn("女");
         Assert.assertEquals("男", b.getSex(1));
         Assert.assertEquals("女", b.getSex(2));
         //对于没有预设的情况会返回默认值
@@ -86,10 +88,10 @@ public class MockitoTest {
 
     @Test
     public void with_upspecified_arguments() {
-        List list = Mockito.mock(List.class);
+        List list = mock(List.class);
         //匹配任意参数
-        Mockito.when(list.get(Mockito.anyInt())).thenReturn(1);
-        Mockito.when(list.contains(Mockito.argThat(new IsValid()))).thenReturn(true);
+        when(list.get(anyInt())).thenReturn(1);
+        when(list.contains(argThat(new IsValid()))).thenReturn(true);
 
         Assert.assertEquals(1, list.get(1));
         Assert.assertEquals(1, list.get(999));
@@ -98,6 +100,16 @@ public class MockitoTest {
         Assert.assertTrue(!list.contains(3));
 
     }
+
+    @Test
+    public void with_upspecified_arguments2() {
+        Comparator comparator = mock(Comparator.class);
+        comparator.compare("nihao", "h");
+        //如果你使用了参数匹配，那么所有参数都必须通过matchers来匹配
+        //第一项anyString代表任何字符串，第二项 eq("h") 所以compare(*,"h")
+        verify(comparator).compare(anyString(), eq("h"));
+    }
+
 
     class IsValid implements ArgumentMatcher<Object> {
         /*
