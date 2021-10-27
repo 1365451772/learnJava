d63f5e84b6d44c5450999b0939e989f0b34d5f36
sunpeng <sunpeng@example.com>
2021-10-27 18:14:24 +0800

    多线程测试

--- mockito/src/main/java/me.sunpneg/controller/HelloController.java
+++ mockito/src/main/java/me.sunpneg/controller/HelloController.java
@@ -3,6 +3,8 @@ package me.sunpneg.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.*;
+
 /**
  * @author sp
  * @date 2021-10-25 10:38
@@ -10,8 +12,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String hello(){
+    @GetMapping("/test")
+    public String hello() throws InterruptedException {
+        final ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10,
+                30,
+                120,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(100),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
+
+        for (int i = 0; i < 10; i++) {
+            final Future<?> submit = threadPoolExecutor.submit(() -> {
+                try {
+                    System.out.println(Thread.currentThread().getName() + "******" + "执行");
+                    Thread.sleep(10000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            });
+        }
+        threadPoolExecutor.shutdown();
+        threadPoolExecutor.awaitTermination(2,TimeUnit.SECONDS);
+        System.out.println(Thread.currentThread().getName() + "******" + "执行");
         return "hello";
+
     }
 }
